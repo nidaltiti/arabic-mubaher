f8e649fe2cae670a62dc0d48cbf2aac6c8e7eabb
nidal titi <nidal_1984@hotmail.com>
2024-04-22 17:37:14 +0300

    fix  channel

--- plugin.video.arabic-mubaher/list_tvchannel.py
+++ plugin.video.arabic-mubaher/list_tvchannel.py
@@ -18,7 +18,14 @@ class list_tv:
              {"name":"قناة الأردن ","kind":satellite_channel,"country":"الأردن" ,"url":"https://jrtv-live.ercdn.net/jordanhd/jordanhd.m3u8","logo":"http://psce.pw/4w6j8n"},
              {"name":"قناة القدس يوم   ","kind":satellite_channel,"country":"فلسطين" ,"url":"http://live.alqudstoday.tv:8080/hls/alqudstv/alqudstv.m3u8","logo":"https://is.gd/mYHff2"},
              {"name":"قناة سوريا ","kind":satellite_channel,"country":"سوريا" ,"url":"https://svs.itworkscdn.net/syriatvlive/syriatv.smil/playlist_dvr.m3u8","logo":"http://psce.pw/4w6j3k"},
-             {"name":"قناة رؤيا ","kind":satellite_channel,"country":"الأردن" ,"url":"https://roya.daioncdn.net/royatv/royatv.m3u8?app=8c1e143a-39cf-40b4-acb2-e78faa1f322d&ce=3","logo":"https://cdn01.paltoday.ps/ar/thumb/940x610/uploads/images/2022/05/h7DgM.jpg"},
+            
+            
+            
+             {"name":" قناة رؤيا درما","kind":satellite_channel,"country":"الأردن" ,"url":"https://tinyurl.com/25wxkxda","logo":"https://cdn01.paltoday.ps/ar/thumb/940x610/uploads/images/2022/05/h7DgM.jpg"},
+           
+             {"name":" قناة رؤيا كوميدي","kind":satellite_channel,"country":"الأردن" ,"url":"https://tinyurl.com/2cfmuras","logo":"https://cdn01.paltoday.ps/ar/thumb/940x610/uploads/images/2022/05/h7DgM.jpg"},
+
+           
              {"name":"قناة الميادين  ","kind":satellite_channel,"country":"لبنان" ,"url":"https://mdnlv.cdn.octivid.com/almdn/smil:mpegts.stream.smil/chunklist_b3000000_t64NTQwcA==.m3u8","logo":"https://media.almayadeen.tv/uploads/archive/default.png"},
               {"name":"سكاي نيوز عربية ","kind":news_channel,"country":"الإمارات" ,"url":"https://stream.skynewsarabia.com/hls/sna.m3u8","logo":"https://m.eyeofriyadh.com/news_images/2020/01/1c376f37a51aa.jpg"},
               {"name":"الجزيرة ","kind":news_channel,"country":"ps" ,"url":"https://live-hls-web-aja.getaj.net/AJA/index.m3u8","logo":"https://pbs.twimg.com/ext_tw_video_thumb/1458689463547219968/pu/img/gE8Cs-DTUNnqt0cy.jpg:large"},
@@ -34,7 +41,7 @@ class list_tv:
               {"name":"MBC Plus Drama  ","kind":drama_channel,"country":"الحجاز" ,"url":"https://is.gd/dNo23x","logo":"https://is.gd/f0eYGI"},
               {"name":"MBC Masr 1","kind":satellite_channel,"country":"مصر" ,"url":"https://is.gd/zxFFxs","logo":"https://is.gd/pfaX9I"},
               {"name":"MBC Masr 2","kind":satellite_channel,"country":"مصر" ,"url":"https://is.gd/510JqZ","logo":"https://is.gd/a2pbJY"},
-              {"name":"العربي","kind":news_channel,"country":"البريطانيا" ,"url":"https://is.gd/aKdUfQ","logo":"https://is.gd/KA9aeM"},
+              {"name":"العربي","kind":news_channel,"country":"البريطانيا" ,"url":"https://tinyurl.com/22pzd7sb","logo":"https://is.gd/KA9aeM"},
               {"name":"المنار ","kind": satellite_channel,"country":"لبنان" ,"url":"https://is.gd/pK8no5","logo":"https://is.gd/pQTm9I"},
               {"name":"الاقصى  ","kind":"satellite_channel","country":"فلسطين" ,"url":"https://tinyurl.com/2po6q2al","logo":"https://tinyurl.com/2zyl24gk"},
               {"name":"دبي 1 ","kind":satellite_channel,"country":"الإمارات" ,"url":"https://is.gd/ZTkf25","logo":"https://is.gd/9l4hKr"},
